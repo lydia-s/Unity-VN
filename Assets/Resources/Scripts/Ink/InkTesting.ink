5f515280dfffbba1f06c1ce55c4f10f7a048a87a
Lydia <savoullis@live.co.uk>
2019-12-11 21:35:31 +0000

    Fixing order of message posting in ink

--- Assets/Resources/Scripts/Ink/InkTesting.ink
+++ Assets/Resources/Scripts/Ink/InkTesting.ink
@@ -1,15 +1,20 @@
-* start_char1
--> start_char1
+* start_char1-> start_char1
 *start_char2
 ->start_char2
 *start_char3
 -> start_char3
 
-===start_char1==
-* Well well->#Received 
+==start_char1==
+* Well well#Received
+What have we here?
+Something->char1_hello
+* Not now#Received
 ->END
-* Not now->#Received
-->END
+
+==char1_hello==
+*hello
+goodbye#Sent
+Somewhat->END
 
 ==start_char2==
 * ok #Sent
--- Assets/Resources/Scripts/Ink/InkTestingScript.cs
+++ Assets/Resources/Scripts/Ink/InkTestingScript.cs
@@ -9,7 +9,6 @@ public class InkTestingScript : MonoBehaviour
 {
     public TextAsset inkJSON;
     private Story story;
-    public Text textPrefab;
     public Button buttonPrefab;
     public TextMeshProUGUI avatarName;
 
@@ -40,20 +39,16 @@ public class InkTestingScript : MonoBehaviour
     public void RefreshUI()
     {
         EraseUI();
-        Text storyText = Instantiate(textPrefab) as Text;
-
-        string text = LoadStoryChunk();
-
-        List<string> tags = story.currentTags;
+        List<string> tags = story.currentTags;//get tags
         string tag = "";
         if (tags.Count > 0)
         {
             tag = tags[0];
         }
+        string name = avatarName.text;//name of contact
 
-        storyText.text = text;
-        
-        storyText.transform.SetParent(this.transform, false);
+        string loadedText = LoadStoryChunk();//gets a chunk of text before a choice is made
+        GameObject worldClock = GameObject.Find("WorldClock");
         
         foreach (Choice choice in story.currentChoices)
         {
@@ -61,19 +56,24 @@ public class InkTestingScript : MonoBehaviour
             Button choiceButton = Instantiate(buttonPrefab) as Button;
             Text choiceText = choiceButton.GetComponentInChildren<Text>();
             choiceText.text = choice.text;
-
             choiceButton.transform.SetParent(this.transform, false);
             choiceButton.onClick.AddListener(delegate {
-                ChooseStoryChoice(choice);
-                gameObject.GetComponent<GenerateMessage>().CreateMessage(storyText.text, tag);//generate text message(box you see on screen)
-                GameObject worldClock = GameObject.Find("WorldClock");
-                string name = avatarName.text;
-                worldClock.GetComponent<MessageLists>().SaveMessageToList(storyText.text, tag, name);//save message to list(for loading later)
+                ChooseStoryChoice(choice);//this method then calls refreshUI
                 var savedState = story.state.ToJson();
+                PostMessages(loadedText, tag, name);
                 worldClock.GetComponent<MessageLists>().SaveStoryState(name, savedState);
             });
         }
 
+
+    }
+    public void PostMessages(string textBlock, string tag, string name) {
+        GameObject worldClock = GameObject.Find("WorldClock");
+        string[] messages = textBlock.Split('\n');
+        foreach (string message in messages) {
+            gameObject.GetComponent<GenerateMessage>().CreateMessage(message, tag);//generate text message(box you see on screen)
+            worldClock.GetComponent<MessageLists>().SaveMessageToList(message, tag, name);//save message to list(for loading later)
+        }
 
     }
     /**When we start before the player has made any decisions the first set of choices will be chosen
@@ -89,6 +89,7 @@ public class InkTestingScript : MonoBehaviour
         switch (name) {
             case "Helen":
                 ChooseFirstPath(story.currentChoices[0]);
+                
                 break;
             case "John":
                 ChooseFirstPath(story.currentChoices[1]);
@@ -116,10 +117,14 @@ public class InkTestingScript : MonoBehaviour
 
     string LoadStoryChunk()
     {
+        
+
         string text = "";
         if (story.canContinue)
         {
             text = story.ContinueMaximally();
+            //generate message here
+            Debug.Log(text);
         }
         
         return text;
